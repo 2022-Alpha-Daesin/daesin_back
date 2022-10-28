c3edd8aae64a181bb8056d2d08e97506e7094d92
koomchang <leahpar0401@gmail.com>
2022-10-29 05:26:39 +0900

    chore: mysql 설정

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DATABASES_NAME"),
+        'USER': os.environ.get("DATABASES_USER"),
+        'PASSWORD': os.environ.get("DATABASES_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
