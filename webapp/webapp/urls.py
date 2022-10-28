413b7ae0e98dbc562fb13f00e738bff40a4034e2
koomchang <leahpar0401@gmail.com>
2022-10-28 16:40:33 +0900

    add: url 연결 확인

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def ping(request):
+    return HttpResponse('pong')
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", ping),
 ]
