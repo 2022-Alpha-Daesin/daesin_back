a97f9a0cb00d405aa3c621b50f20af03956b0270
koomchang <leahpar0401@gmail.com>
2022-10-29 03:58:31 +0900

    chore: django dotenv 추가

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*scn_&w1n(*y^)y$xy@fp*mh(gk@%i@+nl(o*v8+v5q(7!qx_9"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
