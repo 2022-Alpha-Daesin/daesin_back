a090e18ef95be3ba77856c54771d39d38658b62b
koomchang <leahpar0401@gmail.com>
2022-11-01 14:08:25 +0900

    chore: django jazzmin으로 admin 페이지 수정(추후 필요 시 custom 가능)

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -152,4 +153,11 @@ LOGGING = {
             'level': 'DEBUG',
         },
     },
+}
+
+
+JAZZMIN_SETTINGS = {
+    'site_title': 'Daesin',
+    'site_header': 'Daesin',
+    'site_brand': 'Daesin',
 }
