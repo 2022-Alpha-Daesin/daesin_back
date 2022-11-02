22d166644436b31b6bdedbec0abbdf23cf0a7a17
koomchang <leahpar0401@gmail.com>
2022-11-03 03:53:31 +0900

    chore: docker local 환경 설정

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -130,7 +130,10 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
