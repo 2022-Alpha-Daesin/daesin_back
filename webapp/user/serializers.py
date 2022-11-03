c6ece01879b5eee6279f26bba4c6d867e91c2433
koomchang <leahpar0401@gmail.com>
2022-11-04 02:17:49 +0900

    fix: import 정리

--- webapp/user/serializers.py
+++ webapp/user/serializers.py
@@ -1,11 +1,5 @@
-from dj_rest_auth.serializers import UserDetailsSerializer
-from rest_framework import serializers
-from django.conf import settings
-
-from allauth.account.adapter import get_adapter
-from allauth.account.utils import setup_user_email
-
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
 
 
 class CustomRegisterSerializer(RegisterSerializer):
