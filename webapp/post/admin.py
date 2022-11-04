67d8fc12ca6893260cf71dc566a35ac2a84d7086
koomchang <leahpar0401@gmail.com>
2022-11-05 05:05:27 +0900

    add: post admin 작성

--- webapp/post/admin.py
+++ webapp/post/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from post.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'type',
+    )
+    list_filter = (
+        'type',
+    )
+    ordering = (
+        'created_at',
+    )
