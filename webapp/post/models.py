8740728190fe61cc315c3c0d82bc24c6141f0b01
koomchang <leahpar0401@gmail.com>
2022-11-05 04:50:12 +0900

    add: post 모델 작성

---
+++ webapp/post/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from user.models import User
+
+
+class Post(models.Model):
+    """Model definition for Post."""
+
+    class Meta:
+        db_table = 'posts'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    POST_TYPE = (
+        ('REVIEW', 'review'),
+        ('CLUBPOST', 'clubpost'),
+        ('ADVERTISEMENT', 'advertisement'),
+    )
+
+    title = models.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='제목'
+    )
+    content = models.TextField(
+        max_length=2000,
+        null=False,
+        verbose_name='내용'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        verbose_name='글쓴이',
+    )
+    type = models.CharField(
+        max_length=20,
+        choices=POST_TYPE,
+        null=True,
+        verbose_name='글 종류',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='생성 일시',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='수정 일시',
+    )
