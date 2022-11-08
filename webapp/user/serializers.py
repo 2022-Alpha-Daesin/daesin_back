bafb651cee2ac5db167a98f8f33982d6b5c6cbf9
Jia <jia5232@naver.com>
2022-11-08 11:55:49 +0900

    fix:user import

--- webapp/user/serializers.py
+++ webapp/user/serializers.py
@@ -1,6 +1,7 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
-from user.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class CustomRegisterSerializer(RegisterSerializer):
