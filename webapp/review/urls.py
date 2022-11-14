b449c65b3e788ce2183a3821b3880504b6b22f54
koomchang <leahpar0401@gmail.com>
2022-11-14 22:49:34 +0900

    fix: review urls import

--- webapp/review/urls.py
+++ webapp/review/urls.py
@@ -1,4 +1,5 @@
-from review.views.review_list_apiview import ReviewListAPIView, ReviewRetrieveAPIView
+from review.views.review_list_apiview import ReviewListAPIView
+from review.views.review_retrieve_apiview import ReviewRetrieveAPIView
 from django.urls import path
 
 urlpatterns = [
