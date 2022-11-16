836ccf6f96530dfce5a602a706c1e7fe760d0993
koomchang <leahpar0401@gmail.com>
2022-11-16 23:16:03 +0900

    fix: requirements.txt 에서 uwsgi 삭제

--- webapp/review/serializers/review_serializer.py
+++ webapp/review/serializers/review_serializer.py
@@ -9,19 +9,18 @@ class ReviewSerializer(ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ['id','post']
+        fields = ['id', 'post']
 
     def create(self, validated_data):
         post_data = validated_data.pop('post')
-        post = Post.objects.create(**post_data, type = validated_data.pop('type'),  author = validated_data.pop('author'))
-        review = self.Meta.model._default_manager.create(post = post)
+        post = Post.objects.create(**post_data, type=validated_data.pop('type'), author=validated_data.pop('author'))
+        review = self.Meta.model._default_manager.create(post=post)
         return review
-    
+
     def update(self, instance, validated_data):
         post_data = validated_data.pop('post')
         post = instance.post
-        post.title = post_data.get('title',post.title)
-        post.content = post_data.get('content',post.content)
-        post.save(update_fields=['title','content','updated_at'])
+        post.title = post_data.get('title', post.title)
+        post.content = post_data.get('content', post.content)
+        post.save(update_fields=['title', 'content', 'updated_at'])
         return instance
-    
