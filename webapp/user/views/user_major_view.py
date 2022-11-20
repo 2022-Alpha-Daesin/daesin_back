285d47528c9911f02210060d0fdfd7c376ed8cc8
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2022-11-21 00:59:47 +0900

    fix: user 안넣고 id 넣으니까 안돼지...

--- webapp/user/views/user_major_view.py
+++ webapp/user/views/user_major_view.py
@@ -7,7 +7,5 @@ class UserMajorViewSet(ModelViewSet):
     queryset = UserMajor.objects.all()
     serializer_class = UserMajorSerializer
 
-    # usermajor추가하는거 admin에선 잘 되는데 postman에서는 잘 안됨
-    # 형식의 문제인가..?
-    # def perform_create(self, serializer): # 이슈 발생
-    #     serializer.save(user_id=self.request.user.id)
+    def perform_create(self, serializer): 
+        serializer.save(user= self.request.user)
