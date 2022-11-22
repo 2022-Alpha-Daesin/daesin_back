c8d0b3b9a2516f57e0942e6070bbbe10ee653478
koomchang <leahpar0401@gmail.com>
2022-11-22 19:01:49 +0900

    fix: entrypoint

--- webapp/docker-entrypoint.production.sh
+++ webapp/docker-entrypoint.production.sh
@@ -4,4 +4,4 @@ python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn webapp.config.wsgi:application --bind 0.0.0.0:8000
