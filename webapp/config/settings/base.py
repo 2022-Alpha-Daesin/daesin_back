e6112eea0674e438a4f34fe74b34be2b97bdafec
koomchang <leahpar0401@gmail.com>
2022-11-22 21:09:27 +0900

    fix

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -227,7 +227,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "staticfiles/"
+STATIC_URL = "static/"
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
