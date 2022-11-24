208295cacd456c0f960b92b2a865232685ffecec
koomchang <leahpar0401@gmail.com>
2022-11-24 20:46:00 +0900

    add: 일본학과 학사공지 크롤링

--- webapp/notice/department_notice_crawling.py
+++ webapp/notice/department_notice_crawling.py
@@ -219,4 +219,23 @@ def history():
     return {"result": result}
 
 
-print(history())
+def japan():
+    result = []
+    baseUrl = 'https://cha.kookmin.ac.kr/japan/japan_notice/'
+    res = requests.get(baseUrl)
+    soup = bs(res.content, 'html.parser')
+    notice = soup.find_all('td', class_='subject title')[:5]
+    for notice_id, n in enumerate(notice):
+        notice_title = escape_ansi(n.get_text())
+        notice_link = baseUrl + n.find('a', href=True)['href']
+        notice_obj = {
+            'id': notice_id + 1,
+            'major': "일본학과",
+            'title': notice_title,
+            'url': notice_link,
+        }
+        result.append(notice_obj)
+    return {"result": result}
+
+
+print(japan())
--- webapp/notice/views/notice_list_apiview.py
+++ webapp/notice/views/notice_list_apiview.py
@@ -111,6 +111,25 @@ class NoticeListAPIView(ListAPIView):
             result.append(notice_obj)
         return {"result": result}
 
+    # 일본학과
+    def japan(self):
+        result = []
+        baseUrl = 'https://cha.kookmin.ac.kr/japan/japan_notice/'
+        res = requests.get(baseUrl)
+        soup = bs(res.content, 'html.parser')
+        notice = soup.find_all('td', class_='subject title')[:5]
+        for notice_id, n in enumerate(notice):
+            notice_title = escape_ansi(n.get_text())
+            notice_link = baseUrl + n.find('a', href=True)['href']
+            notice_obj = {
+                'id': notice_id + 1,
+                'major': "일본학과",
+                'title': notice_title,
+                'url': notice_link,
+            }
+            result.append(notice_obj)
+        return {"result": result}
+
     # 정치외교학과
     def politics(self):
         result = []
