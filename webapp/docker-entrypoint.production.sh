0f485bd5d3e01318c69144724e3169de778c636f
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2022-12-12 00:55:28 +0900

    chore:docker 실행 스크립트에 동아리 세팅 명령어 추가

--- webapp/docker-entrypoint.production.sh
+++ webapp/docker-entrypoint.production.sh
@@ -6,7 +6,7 @@ python manage.py migrate --noinput --settings=config.settings.prod
 python manage.py pull_major --settings=config.settings.prod
 python manage.py pull_menus --settings=config.settings.prod
 python manage.py init_tags --settings=config.settings.prod
-python manage.py init_clubs --settings=config.settings.prod
+python manage.py init_club --settings=config.settings.prod
 python manage.py collectstatic --noinput --settings=config.settings.prod
 
 gunicorn config.wsgi:application --env DJANGO_SETTINGS_MODULE=config.settings.prod --bind 0.0.0.0:8000
